a3437ddb88febaa0de3d10f81996ce8d0db8c3a3
Renato Mendes <renato.mendes.1123@gmail.com>
2016-07-13 09:02:12 -0400

    Adding Front End

---
+++ butterfly-effect-frontend/src/main/java/com/scipionyx/butterflyeffect/frontend/FrontEndApplication.java
@@ -0,0 +1,32 @@
+
+package com.scipionyx.butterflyeffect.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author Renato Mendes
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = { "${butterflyeffect.system.componentscan.basepackages}" })
+
+@Configuration
+@EnableAutoConfiguration
+// @EnableEurekaClient
+public class FrontEndApplication {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndApplication.class, args);
+	}
+
+}
