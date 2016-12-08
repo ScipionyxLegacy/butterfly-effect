ebd72f9b950550ce39ab6dc30f52c90b95b64dad
Renato Mendes <renato.mendes.1123@gmail.com>
2016-12-08 09:41:16 -0500

    Adding Landing page for initial access to the system

---
+++ butterfly-effect-frontend-core/src/main/java/com/scipionyx/butterflyeffect/frontend/core/ui/view/root/LandingPageView.java
@@ -0,0 +1,56 @@
+package com.scipionyx.butterflyeffect.frontend.core.ui.view.root;
+
+import com.scipionyx.butterflyeffect.frontend.core.ui.view.common.AbstractView;
+import com.scipionyx.butterflyeffect.ui.view.MenuConfiguration;
+import com.scipionyx.butterflyeffect.ui.view.MenuConfiguration.Position;
+import com.scipionyx.butterflyeffect.ui.view.ViewConfiguration;
+import com.vaadin.annotations.DesignRoot;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * 
+ * 
+ * 
+ * @version 0.1.0
+ * @author Renato Mendes
+ *
+ */
+@DesignRoot()
+@SpringComponent(value = LandingPageView.VIEW_NAME)
+@SpringView(name = LandingPageView.VIEW_NAME)
+@UIScope
+//
+@ViewConfiguration(title = "Welcome")
+@MenuConfiguration(position = Position.TOP_MAIN, label = "Home", group = "", order = -1)
+public class LandingPageView extends AbstractView {
+
+	public static final String VIEW_NAME = "";
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	@Override
+	public void doEnter(ViewChangeEvent event) {
+	}
+
+	@Override
+	public void doBuild() {
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void doBuildWorkArea(VerticalLayout workAreaPanel) {
+	}
+
+}
