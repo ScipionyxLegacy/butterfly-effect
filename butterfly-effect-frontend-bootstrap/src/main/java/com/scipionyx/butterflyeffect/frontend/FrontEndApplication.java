b25c68a73af76f38bcd7c1cf253b624edf28b1e7
Renato Mendes <renato.mendes.1123@gmail.com>
2016-12-21 18:16:35 -0500

    Fixes

--- butterfly-effect-frontend-bootstrap/src/main/java/com/scipionyx/butterflyeffect/frontend/FrontEndApplication.java
+++ butterfly-effect-frontend-bootstrap/src/main/java/com/scipionyx/butterflyeffect/frontend/FrontEndApplication.java
@@ -1,25 +1,35 @@
 
 package com.scipionyx.butterflyeffect.frontend;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.vaadin.spring.session.redis.VaadinSessionRewriteFilter;
 
 /**
  * 
  * @author Renato Mendes
  *
  */
-@SpringBootApplication
-@ComponentScan(basePackages = { "${butterflyeffect.system.componentscan.basepackages}", })
 
+//
+@SpringBootApplication
+
+//
 @Configuration
 @EnableAutoConfiguration
 
+//
 @EnableDiscoveryClient
+
+//
+@EnableRedisHttpSession
 public class FrontEndApplication {
 
 	/**
@@ -28,6 +38,11 @@ public class FrontEndApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FrontEndApplication.class, args);
+	}
+
+	@Bean
+	public Filter vaadinSessionRewriteFilter() {
+		return new VaadinSessionRewriteFilter();
 	}
 
 }
--- butterfly-effect-frontend/docker/entrypoint.sh
+++ butterfly-effect-frontend/docker/entrypoint.sh
@@ -6,6 +6,7 @@
 ##
 
 exec java \
+	-Dserver.port=$SERVER_PORT \
 	-Dspring.cloud.consul.host=$SPRING_CLOUD_CONSUL_HOST \
 	-Dvaadin.servlet.productionMode=$VAADIN_SERVLET_PRODUCTION_MODE \
 	-jar $PROJECT_ARTIFACTID-bootstrap-$PROJECT_VERSION-exec.jar
